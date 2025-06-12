aec0c3bbfaa7a895ef61a758813b4e23e8299264
Pavlo Nikolaiev <109036070+panik10@users.noreply.github.com>
2025-06-12 10:57:21 +0200

    - Added breadcrumbs - Fixed admin panel bugs
    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,35 @@
-import { defineConfig } from 'vite'
-import path from "path";
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://backend-api.space',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''), // Перенаправляйте всі запити
-  //     },
-  //   },
-  // },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler' // or "modern"
-      }
-    }
-  },
-  plugins: [react()],
-  base: "/Charity_website",
-  resolve: {
-    alias: {
-    "@": path.resolve(__dirname, "./src"),
-  },
-},
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_LOCAL_API_BASE_URL || 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+    plugins: [react()],
+    base: '/', 
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'process.env.VITE_RENDER_API_BASE_URL': JSON.stringify(env.VITE_RENDER_API_BASE_URL),
+    },
+  };
+});
